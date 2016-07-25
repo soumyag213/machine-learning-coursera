6a5ccd551a9e8e3adc83277f5f41423eaabbbc2c
soumya <soumyag213@gmail.com>
2016-07-25 12:54:42 +0530

    fifth assignment

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -1,7 +1,7 @@
 function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
 %LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear 
 %regression with multiple variables
-%   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the 
+%   [J, grad] = LINEARREGCOSTcdFUNCTION(X, y, theta, lambda) computes the 
 %   cost of using theta as the parameter for linear regression to fit the 
 %   data points in X and y. Returns the cost in J and the gradient in grad
 
@@ -19,17 +19,20 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+%size(theta)
+J = sum((X*(theta) - y).^2);
+temp = theta;
+temp(1,:)=0;
+reg = lambda*sum(temp.*temp);
+J = (0.5/m)*(J+reg);
 
 
-
-
-
-
-
-
-
-
-
+K= J;
+%size(grad)
+grad(1,1) = (1/m)*sum( ((X*theta)-y) .* (X(:,1)) );
+for K = 2:size(theta)
+grad(K,1) = (1/m)*sum( ((X*theta)-y) .* (X(:,K)) ) + (lambda/m)*theta(K,1);
+endfor
 % =========================================================================
 
 grad = grad(:);

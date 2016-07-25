547f9f1c046c90cb3ebc7f31abde55e86418728f
soumya <soumyag213@gmail.com>
2016-07-25 13:35:41 +0530

    add fifth assignment

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -53,10 +53,11 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
+val_size = size(Xval,1);
 for i = 1:m
-	theta = trainLinearReg(X(1:i,:), y(1:i), 0);	
-	error_train(i) = linearRegCostFunction(X(1:i,:), y(1:i), theta, 0);
-	error_val(i) = linearRegCostFunction(Xval, yval, theta,0);
+	theta = trainLinearReg( [ones(i,1) X(1:i,:)], y(1:i), 0);	
+	[error_train(i),grad] = linearRegCostFunction([ones(i,1) X(1:i,:)], y(1:i), theta, 0);
+	[error_val(i),grad] = linearRegCostFunction([ ones(val_size,1) Xval], yval, theta,0);
 endfor
 
 
